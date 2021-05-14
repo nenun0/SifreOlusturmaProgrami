29d489e12b78d04da0eeae881a8a0e493a4b7f60
Nenun0-Web <84199716+josephtr0-web@users.noreply.github.com>
2021-05-15 02:53:31 +0300

    Password Oluşturucu

---
+++ SifreOlusturmaProgramı/GorselTabanliVizeProjesi/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace GorselTabanliVizeProjesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.ForestGreen;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.button1.Location = new System.Drawing.Point(96, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SIFRE OLUSTUR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(64, 22);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(248, 54);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(129, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Sifre Uzunlugu :";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(393, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(149, 262);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = " ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(400, 277);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Olusturulan Sifreler";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(403, 300);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(554, 325);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SifreOlusturmaProgramı/GorselTabanliVizeProjesi/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GorselTabanliVizeProjesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        int olusturlanSifreSayisi = 0; // olusturdugum sifre sayilarini tutmak icin bir degisken.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            textBox1.Text = " "; //butona tiklandiginda textboxun icini temizlemek icin
+            Random rastgele = new Random(); // rastgale sayi olusturmak icin random classini tanimladim.
+            string sifre = ""; 
+
+
+            //Ilk haneyi buyuk ya da kucuk harf ile yapmak icin.
+            int sayiUreteci1 = rastgele.Next(1, 3); //1 ya da 2 sayilarindan rastgele birini olusturmak 
+
+            if (sayiUreteci1 == 1) //olusan sayi 1 ise sifrenin ilk hanesi buyuk harf olacak
+            {
+                char harfUret = Convert.ToChar(rastgele.Next(65, 90)); //buyuk harf olusturmak icin
+                sifre += harfUret.ToString();
+            }
+            else // olusan sayi 2 ise sifrenin ilk hanesi kucuk sayi olacak
+            {
+                char harfUret = Convert.ToChar(rastgele.Next(97, 123)); //kuck harf olusturmak icin
+                sifre += harfUret.ToString();
+            }
+
+
+            //2-11 hanelerini rastgele harf,karakter ve sayi yapmak icin.
+            //oncelikle 3 tane rastgele sayi urettim.
+            int random = rastgele.Next(0, 3);
+            int random2 = rastgele.Next(3, 6);
+            int random3 = rastgele.Next(6, 9);
+
+            for (int i = 0; i < 10; i++)
+            {
+                //bu if kisminda urettigim rastgele sayi bunlardan birine girdigi zaman o kosulu saglayacak
+                //bunu yapma sebebim 2. ve 11. haneleri arasindan birinde mutlaka harf ve sayi yazdirmak.
+                if (i == random)
+                {
+                    sifre += Convert.ToChar(rastgele.Next(65, 91)); // mutlaka bir buyuk harf uretip sifre stringine eklemek icin
+                    continue;
+                }
+                
+                if (i == random2)
+                {
+                    sifre += Convert.ToChar(rastgele.Next(97, 123)); //mutlaka bir kucuk uretip sifre stringine eklemek icin
+                    continue;
+                }
+                
+                if (i == random3)
+                {
+                    sifre += Convert.ToChar(rastgele.Next(48, 58)); //mutlaka bir rakam uretip sifre stringine eklemek icin
+                    continue;
+
+                }
+
+                // ve bundan sonrasi rastgele sayi, harf ve ozel karakter uretip sifre stringime eklemek icin
+                int sayi = rastgele.Next(0, 4); 
+
+                if (sayi == 0)
+                {
+                    sifre += Convert.ToChar(rastgele.Next(65, 91)); //buyuk harf uretip sifre stringine eklemek icin
+
+                }
+
+                else if(sayi == 1)
+                {
+                    sifre += Convert.ToChar(rastgele.Next(97, 123)); //kucuk harf uretip sifre stringine eklemek icin
+
+                }
+
+                else if (sayi == 2 )
+                { 
+                    sifre += Convert.ToChar(rastgele.Next(48, 58)); //rakamlari uretip sifre stringine eklemek icin
+
+                }
+
+                else
+                {
+                    sifre += Convert.ToChar(rastgele.Next(33, 48)); //ozel karakterler uretip sifre stringine eklemek icin
+                    
+                }
+            }
+
+            
+            //Son haneyi ozel karakter ile bitirmek icin.
+            int sayiUreteci3 = rastgele.Next(1, 3);
+
+            if (sayiUreteci3 == 1)
+            {
+                char ozelKarakterUret = Convert.ToChar(rastgele.Next(33, 48));
+                sifre += ozelKarakterUret.ToString();
+            }
+            else
+            {
+                char ozelKarakterUret = Convert.ToChar(rastgele.Next(58, 65));
+                sifre += ozelKarakterUret.ToString();
+
+            }
+
+
+            richTextBox1.Text += olusturlanSifreSayisi +  "- " + sifre +  "\n"; //textboxa olusan sifreleri sirasiyla ve kacinci sirada olduklarini yazdirma
+
+            textBox1.Text += sifre; // textboxa sifreyi yazdirma
+            
+            label1.Text = "Sifre Uzunlugu :" + sifre.Length.ToString(); // label1 e sifre uzunlugunu yazdirma
+           
+            olusturlanSifreSayisi++; //sifre olustur butonuna her tiklandiginda olusturulan sifre sayisi degiskenimizi 1 arttiriyorum
+            
+            label3.Text = "Olusturulan sifre sayisi :" +  olusturlanSifreSayisi; // label3 e olusturulan sifre sayisini yazdirma
+
+
+        }
+    }
+}
